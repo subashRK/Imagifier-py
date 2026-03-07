28e3dcaa5aeb421883938359ac15ab00e0063e19
subashRK <subashkohila24092007@gmail.com>
2026-03-07 22:35:03 +0530

    did some tests, everything's actually working!

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from algo import first_fit, best_fit
 input_img_path, target_img_path = None, None
 
 #256, 144; 0.5, 4
-def process_img(input_img, target_img, rad = 0.6, factor = 10):
+def process_img(input_img, target_img, rad = 1, factor = 10):
     print("Cropping image!")
     input_img, target_img = truncate_img(input_img, target_img)
     print("Finished cropping!")
@@ -20,9 +20,9 @@ def process_img(input_img, target_img, rad = 0.6, factor = 10):
     print("Finished compressing!")
 
     # output_img = first_fit(input_img, target_img, rad)
-    output_img = best_fit(input_img, target_img, rad)
+    # output_img = best_fit(input_img, target_img, rad)
 
-    cv2.imwrite(f"./output/{input_img_path.split("/")[-1]}-{target_img_path.split("/")[-1]}", numpy.array(output_img))
+    cv2.imwrite(f"./output/{input_img_path.split("/")[-1]}-{target_img_path.split("/")[-1]}", numpy.array(target_img))
 
 def main(args):
     if len(args) < 2:
